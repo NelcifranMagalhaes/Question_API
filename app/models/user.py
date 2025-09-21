d7dffbf9cb18bfc8dcfa941e64754f8968f75dbf
NelcifranMagalhaes <nelcifranpires@gmail.com>
2025-09-21 18:03:36 -0300

    Add password_hash field to User and UserCreate models

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from pydantic import Field
 class User(Document):
     name: str = Field(..., max_length=100)
     email: str
+    password_hash: str =Field(..., max_length=100)  
 
     class Settings:
         name = "users"
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     name: str
     email: str
+    password_hash: str
 
 class UserResponse(BaseModel):
     id: str
